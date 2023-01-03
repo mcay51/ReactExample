f62f87c59600c18199c73130b5f233a398ee631c
mustafa.cay <mustafa.cay@hmb.gov.tr>
2023-01-03 13:45:33 +0300

    my-module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 import slugify from "slugify";
-import {hello,topla,cikar} from "./my-module.js";
+import hello, {topla,cikar, text,user,users} from "./my-module.js";
 const title=slugify("some string lorem ipsum","-");
 
 hello("Mustafa")
 console.log(topla(10,15));
+console.log(cikar(6,2));
+console.log(text);
+console.log(user);
+console.log(users);
--- my-module.js
+++ my-module.js
@@ -1,7 +1,33 @@
-const topla=(a,b)=> a+b;
-const hello=name=>{
+export default  name=>{
     console.log("Hello "+name); 
     console.log(`Hello Node ${name}`); 
 };
-const cikar=(a,b)=>a-b;
-export {topla,hello,cikar};
+ 
+ const topla=(a,b)=> a+b;
+
+ const cikar=(a,b)=>a-b;
+ const text='düz metin';
+ const user={
+    name:"MUSTAFA",
+    surname:"ÇAY"
+};
+
+ const users=[{
+    name:"MUSTAFA",
+    surname:"ÇAY"
+},{
+    name:"AHMET",
+    surname:"ERTAN"
+},
+{
+    name:"BÜLENT",
+    surname:"TÜRKMEN"
+},
+];
+export {
+    topla,
+    text,
+    cikar,
+    user,
+    users
+};
