e464bcc266d4a4a6a520fb32aa9f5c91bedfef2f
mustafa.cay <mustafa.cay@hmb.gov.tr>
2023-01-03 13:26:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import slugify from "slugify";
+import {hello,topla,cikar} from "./my-module.js";
+const title=slugify("some string lorem ipsum","-");
+
+hello("Mustafa")
+console.log(topla(10,15));
---
+++ my-module.js
@@ -0,0 +1,7 @@
+const topla=(a,b)=> a+b;
+const hello=name=>{
+    console.log("Hello "+name); 
+    console.log(`Hello Node ${name}`); 
+};
+const cikar=(a,b)=>a-b;
+export {topla,hello,cikar};
