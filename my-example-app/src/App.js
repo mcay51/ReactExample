711a0e6b800da938c9a57dc50f98a46ed3a42420
mustafa.cay <mustafa.cay@hmb.gov.tr>
2023-01-04 15:58:08 +0300

    Header Component

--- my-example-app/src/App.js
+++ my-example-app/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+const
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
+      <Header></Header>
+      <p className="xyz">
+        Tempor ut deserunt aliquip commodo ex labore est nostrud excepteur amet incididunt nulla labore culpa. Labore culpa ea amet eu. Veniam anim occaecat occaecat exercitation reprehenderit ad sint consectetur quis enim anim aute nostrud. Sint pariatur enim eu in dolore. Laborum voluptate ea exercitation do duis nisi aute exercitation Lorem exercitation cupidatat voluptate.
+      </p>
+      <label htmlFor="myInput">
+        Name : 
+        <input id="myInput"></input>
+      </label>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
