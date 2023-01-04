efda54736d3932a9ec99a5ff30640016bd509965
mustafa.cay <mustafa.cay@hmb.gov.tr>
2023-01-04 16:50:51 +0300

    degişken tanımı ve if koşulu

--- my-example-app/src/App.js
+++ my-example-app/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-
+const name="Mustafa";
+const surname="ÇAY";
+const isLoggedIn=false;
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,12 @@ function App() {
       <p className="xyz">
         Tempor ut deserunt aliquip commodo ex labore est nostrud excepteur amet incididunt nulla labore culpa. Labore culpa ea amet eu. Veniam anim occaecat occaecat exercitation reprehenderit ad sint consectetur quis enim anim aute nostrud. Sint pariatur enim eu in dolore. Laborum voluptate ea exercitation do duis nisi aute exercitation Lorem exercitation cupidatat voluptate.
       </p>
+      <h1>{name}  {surname}</h1>
+      <h1>{isLoggedIn && ` Benim Adım ${name}  ${surname} `}</h1>
+      <h1>{!isLoggedIn && "Giriş Yapılmadı..."}</h1>
+
+      <h1>{isLoggedIn ? ` Benim Adım ${name}  ${surname} `: "Giriş Yapmadınız .... "}</h1>
+    
       <label htmlFor="myInput">
         Name : 
         <input id="myInput"></input>
